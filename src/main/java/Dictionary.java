ff902d6527c6b3e9162a9ad19ef9af0e859fada2
jdhickey <jack.hickey@rogers.com>
2023-02-03 13:21:08 -0500

    added simple input method

---
+++ src/main/java/Dictionary.java
@@ -0,0 +1,110 @@
+
+import java.io.IOException;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/*
+dictionary format:
+"mí" : [
+      {
+        "pronunciation" : "mi",
+        "pos" : "aux",
+        "strong" : true,
+        "meanings": ["To have an obligation to, should"],
+        "translations": ["should"],
+        "related": [""]
+      },
+      {
+        "pronunciation" : "mi",
+        "pos" : "pronoun",
+        "strong" : true,
+        "meanings": ["1PL inclusive pronoun"],
+        "translations": ["we"],
+        "related": ["réanmí"]
+      }
+    ]
+ */
+
+public class Dictionary {
+    public static void main(String[] args) {
+        JSONObject dictionary = openDictionary();
+
+        System.out.println(newWord());
+    }
+
+    static JSONObject newWord() {
+        JSONObject entry = new JSONObject();
+        System.out.println("Enter word.");
+        String word = readString();
+
+        JSONObject content = new JSONObject();
+
+        System.out.println("Enter pronunciation");
+        content.put("pronunciation", readString());
+
+        System.out.println("Enter part of speech");
+        content.put("pos", readString());
+
+        System.out.println("Enter strength");
+        content.put("strong", readBoolean());
+
+        System.out.println("Enter meanings");
+        content.put("meanings", readArray());
+
+        System.out.println("Enter translations");
+        content.put("translations", readArray());
+
+        System.out.println("Enter related words");
+        content.put("related", readArray());
+
+        entry.put(word, content);
+
+        return entry;
+    }
+
+    static JSONObject openDictionary() {
+        File file = new File("src/main/dictionary.json");
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
+            return new JSONObject(content);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    static String readString() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+    static boolean readBoolean() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                return Boolean.parseBoolean(scanner.nextLine());
+            } catch (RuntimeException e) {}
+        }
+    }
+    static ArrayList<String> readArray() {
+        ArrayList<String> list = new ArrayList<>();
+
+        String word = readString();
+
+        while (!word.equals("")) {
+            list.add(word);
+            word = readString();
+        }
+
+        return list;
+    }
+}
