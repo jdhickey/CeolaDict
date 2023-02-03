19916a8c7686cd5785ad22a81326ed5465f24a81
jdhickey <jack.hickey@rogers.com>
2023-02-03 14:42:43 -0500

    can now add words to C2E part of JSON file

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
