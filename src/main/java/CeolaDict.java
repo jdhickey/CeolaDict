46cf639dd4d7092db301b99ca6ed6308b42925a9
jdhickey <jack.hickey@rogers.com>
2023-02-18 23:41:04 -0500

    Fix bug where related items dissapear after reloading file.

--- src/main/java/CeolaDict.java
+++ src/main/java/CeolaDict.java
@@ -35,49 +35,7 @@ public class CeolaDict {
      * @param args The command line arguments.
      */
     public static void main(String[] args) {
-        dictionaryJSON = openDictionary();
-
-        Iterator<String> keys = dictionaryJSON.keys();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            JSONObject content = dictionaryJSON.getJSONObject(key);
-
-            for (Object item : content.getJSONArray("entries")) {
-                JSONObject object = (JSONObject) item;
-                Word word = new Word(
-                        key,
-                        object.getString("pronunciation"),
-                        makeArrayList(object.getJSONArray("part of speech")),
-                        makeArrayList(content.getJSONArray("related")),
-                        makeArrayList(object.getJSONArray("translations")),
-                        makeArrayList(object.getJSONArray("meanings")),
-                        object.getBoolean("weak")
-                );
-                dictionary.add(word);
-
-                for (String related : makeArrayList(content.getJSONArray("related"))) {
-                    try {
-                        if (!relatedMap.get((word.getWord())).contains(related)) {
-                            relatedMap.get(word.getWord()).add(related);
-                        }
-                    } catch (RuntimeException re) {
-                        ArrayList<String> newArr = new ArrayList<>();
-                        newArr.add(related);
-                        relatedMap.put(word.getWord(), newArr);
-                    }
-                }
-
-                if (content.getJSONArray("related").length() > 1) {
-                    Collections.sort(relatedMap.get(word.getWord()));
-                } else {
-                    relatedMap.put(word.getWord(), new ArrayList<>());
-                }
-
-                word.setRelated(relatedMap.get(word.getWord()));
-            }
-        }
-
-        Collections.sort(dictionary);
+        openDictionary("src/main/dictionary.json");
         Window.main();
     }
 
@@ -85,15 +43,54 @@ public class CeolaDict {
      * This opens the dictionary and returns the JSON Object representing it.
      * @return the dictionary.json file content. If the file fails to open, null is returned.
      */
-    static JSONObject openDictionary() {
-        File file = new File("src/main/dictionary.json");
+    static void openDictionary(String pathname) {
+        File file = new File(pathname);
         try {
-            String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
-            return new JSONObject(content);
+            dictionaryJSON = new JSONObject(new String(Files.readAllBytes(Paths.get(file.toURI()))));
 
+            Iterator<String> keys = dictionaryJSON.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                JSONObject content = dictionaryJSON.getJSONObject(key);
+
+                for (Object item : content.getJSONArray("entries")) {
+                    JSONObject object = (JSONObject) item;
+                    Word word = new Word(
+                            key,
+                            object.getString("pronunciation"),
+                            makeArrayList(object.getJSONArray("part of speech")),
+                            makeArrayList(content.getJSONArray("related")),
+                            makeArrayList(object.getJSONArray("translations")),
+                            makeArrayList(object.getJSONArray("meanings")),
+                            object.getBoolean("weak")
+                    );
+                    dictionary.add(word);
+
+                    for (String related : makeArrayList(content.getJSONArray("related"))) {
+                        try {
+                            if (!relatedMap.get((word.getWord())).contains(related)) {
+                                relatedMap.get(word.getWord()).add(related);
+                            }
+                        } catch (RuntimeException re) {
+                            ArrayList<String> newArr = new ArrayList<>();
+                            newArr.add(related);
+                            relatedMap.put(word.getWord(), newArr);
+                        }
+                    }
+
+                    if (content.getJSONArray("related").length() > 0) {
+                        Collections.sort(relatedMap.get(word.getWord()));
+                    } else {
+                        relatedMap.put(word.getWord(), new ArrayList<>());
+                    }
+
+                    word.setRelated(relatedMap.get(word.getWord()));
+                }
+            }
+
+            Collections.sort(dictionary);
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
     }
 
@@ -101,7 +98,7 @@ public class CeolaDict {
      * Parses the dictionary ArrayList back into a JSON object and writes it to the dictionary.json file.
      * Copies the previous content of the dictionary.json file into archive.json, just in case.
      */
-    static void writeDictionary() {
+    static void writeDictionary(String path) {
         JSONObject dictOut = new JSONObject();
 
         for (Word word : dictionary) {
@@ -122,11 +119,11 @@ public class CeolaDict {
         }
 
         try {
-            File old = new File("src/main/dictionary.json");
+            File old = new File(path);
             File archive = new File("src/main/archive.json");
             old.renameTo(archive);
 
-            FileWriter currentWriter = new FileWriter("src/main/dictionary.json");
+            FileWriter currentWriter = new FileWriter(path);
             currentWriter.write(dictOut.toString(4));
             currentWriter.close();
         } catch (IOException e) {
